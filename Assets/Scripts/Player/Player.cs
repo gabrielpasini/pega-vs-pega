7be3b472aa5f4ff2fbb45b28322e13e91658b7cb
gabrielpasini <gabrielpasini@outlook.com.br>
2024-08-04 13:50:40 -0300

    :construction: implementa belly e spin

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    public float speed;
+    public float slideSpeed;
+    public float spinDuration;
+    public float slideDuration;
+    public float bellyStunDuration;
     public float jumpForce;
+    public float jumpSlidingForce;
+    public bool isSpinning;
+    public bool isSliding;
+    public bool isBellying;
     public bool isOnGround;
     private bool canJump;
     private bool canPress = true;
     public float actualJump = 0;
     public float maxJumps;
 
-    public float speed;
     void Start() {
         canJump = true;
         rb = GetComponent<Rigidbody2D>();
@@ -26,16 +34,60 @@ public class Player : MonoBehaviour {
             if (canJump && actualJump < maxJumps) {
                     actualJump++;
                     rb.velocity = new Vector2(rb.velocity.x, 0f);
-                    rb.AddForce(new Vector2(0f, jumpForce));
+                    if (isSliding) {
+                        rb.AddForce(new Vector2(0f, jumpSlidingForce));
+                    } else {
+                        rb.AddForce(new Vector2(0f, jumpForce));
+                    }
             } else {
                 canJump = false;
             }
         }
+        if (!isSliding && Input.GetKeyDown(KeyCode.F)) {
+            if (isOnGround && rb.velocity.x != 0) {
+                isSliding = true;
+                rb.AddForce(new Vector2(slideSpeed * moveX, 0f));
+                StartCoroutine("stopSlide");
+            }
+            if (!isBellying && !isOnGround) {
+                isBellying = true;
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+                StartCoroutine("performBelly");
+            }
+        }
+        if (!isSpinning && Input.GetKeyDown(KeyCode.Q)) {
+            isSpinning = true;
+            StartCoroutine("stopSpinning");
+        }
 
-        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+        if (!isSliding && !isBellying) {
+            rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+        }
     }
 
-    public IEnumerator JumpDebounce() {
+    private IEnumerator stopSlide() {
+        yield return new WaitForSeconds(slideDuration);
+        isSliding = false;
+    }
+    private IEnumerator stopSpinning() {
+        yield return new WaitForSeconds(spinDuration);
+        isSpinning = false;
+    }
+    private IEnumerator performBelly() {
+        rb.velocity = new Vector2(0f, rb.velocity.y);
+        rb.AddForce(new Vector2(0f, jumpForce * 0.5f));
+        yield return new WaitForSeconds(0.2f);
+        rb.AddForce(new Vector2(0f, -jumpForce));
+    }
+
+    private IEnumerator stopBelly() {
+        yield return new WaitForSeconds(bellyStunDuration);
+        isBellying = false;
+    }
+
+
+
+    private IEnumerator JumpDebounce() {
         canPress = false;
         yield return new WaitForSeconds(0.05f);
         canPress = true;
@@ -54,6 +106,10 @@ public class Player : MonoBehaviour {
             actualJump = 0;
             canJump = true;
             isOnGround = true;
+            if (isBellying) {
+                StartCoroutine("stopBelly");
+            }
+
         }
     }
 
--- Assets/Scripts/Player/SpriteAnimation.cs
+++ Assets/Scripts/Player/SpriteAnimation.cs
@@ -11,30 +11,54 @@ public class SpriteAnimation : MonoBehaviour {
     private Player player;
 
     void Update() {
-        float speedX = this.rigidbody.velocity.x;
+        if (this.player.isSpinning && !this.player.isBellying && !this.player.isSliding) {
+            this.animator.SetBool("PlayerSpinning", true);
+        } else {
+            this.animator.SetBool("PlayerSpinning", false);
+        }
         if (this.player.isOnGround) {
+            float speedX = this.rigidbody.velocity.x;
+            if (!this.player.isBellying) {
+                this.animator.SetBool("PlayerBellying", false);
+            }
             if (speedX != 0) {
-                this.animator.SetBool("PlayerRunning", true);
+                if (this.player.isSliding) {
+                    this.animator.SetBool("PlayerSliding", true);
+                    this.animator.SetBool("PlayerRunning", false);
+                } else {
+                    this.animator.SetBool("PlayerSliding", false);
+                    this.animator.SetBool("PlayerRunning", true);
+                }
             } else {
                 this.animator.SetBool("PlayerRunning", false);
+                this.animator.SetBool("PlayerSliding", false);
             }
             this.animator.SetBool("PlayerJumping", false);
             this.animator.SetBool("PlayerDoubleJumping", false);
             this.animator.SetBool("PlayerFalling", false);
         } else {
             float speedY = this.rigidbody.velocity.y;
-            if (speedY > 0) {
-                if (this.player.actualJump == this.player.maxJumps) {
-                    this.animator.SetBool("PlayerDoubleJumping", true);
-                } else {
-                    this.animator.SetBool("PlayerJumping", true);
-                }
+            if (this.player.isBellying) {
+                this.animator.SetBool("PlayerBellying", true);
                 this.animator.SetBool("PlayerFalling", false);
-            } else if (speedY < 0) {
                 this.animator.SetBool("PlayerJumping", false);
                 this.animator.SetBool("PlayerDoubleJumping", false);
-                this.animator.SetBool("PlayerFalling", true);
+            } else {
+                if (speedY > 0) {
+                    if (this.player.actualJump == this.player.maxJumps) {
+                        this.animator.SetBool("PlayerDoubleJumping", true);
+                    } else {
+                        this.animator.SetBool("PlayerJumping", true);
+                    }
+                    this.animator.SetBool("PlayerFalling", false);
+                    this.animator.SetBool("PlayerSliding", false);
+                } else if (speedY < 0) {
+                    this.animator.SetBool("PlayerFalling", true);
+                    this.animator.SetBool("PlayerJumping", false);
+                    this.animator.SetBool("PlayerDoubleJumping", false);
+                }
             }
+            this.animator.SetBool("PlayerRunning", false);
         }
     }
 }
